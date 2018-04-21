b2a17978b2718967389862ee00dc04421d144a10
LauraMilenaRB <lauramilena.rbe@gmail.com>
2018-04-21 02:05:59 -0500

    commit2

--- prueba.py
+++ prueba.py
@@ -12,30 +12,32 @@ import time
     
 data=[1,2,3,4]
 
-def DHT11(sensor, pin):
+def DHT(sensor, pin):
     
     while True:
         temp, hum = Adafruit_DHT.read_retry(sensor, pin)
         data[0]=round(temp,1)
         data[1]=round(hum,1)
+        print(temp,hum)
         RestRequests()
-        time.sleep(5)
+        break
     
     
 def RestRequests():
     
     # Definimos la URL
-    url = "https://192.168.0.15/"
-    # Solicitamos los datos del usuario
-    # Definimos la cabecera y el diccionario con los datos
+    url = "http://192.168.0.15/"
+    print("mdfknfoindiofnvfr")
     header = {'Content-type': 'application/json'}
     dat = ('User:{"email": "%s","password":"%s","name":"%s","image":"%s","confirmPassword":"%s"}')%("email","pdw", "name","image","pdw")
     petition = requests.post(url+"/users/", headers = header, data = dat)
+    print("mdfknfoindiofnvfr2")
     if petition.status_code == 200:
         print(petition.text)
+    print("mdfknfoindiofnvfr3")
         
 def main():
-    t = threading.Thread(target=DHT11(11,2))
+    t = threading.Thread(target=DHT,args=(Adafruit_DHT.DHT11,2))
     t.start()
     if __name__:"main"
 
